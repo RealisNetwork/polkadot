9ffc68da474cd7cf93ebc6e08a54bae350e79239
BeginArtem <lartem.1205@gmail.com>
2022-04-27 14:47:57 +0300

    fix: remove beefy-mmr from .toml

--- scripts/run_charlie.sh
+++ scripts/run_charlie.sh
@@ -3,6 +3,6 @@
 --validator \
 --base-path /tmp/relay/charlie \
 --chain chain-spec.json \
---bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWPD7C266U5kQJrs3KMHZFMZHL9jQvCQaySnkTT6TR7wrn \
+--bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWMeN4sKXgsKaATmUFw4rsXrdTwkekuQk2cZPcnKLfh3ME \
 --port 30335 \
 --ws-port 9946
